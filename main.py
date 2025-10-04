8a0c562d8142439e58f57fd0f58d33af59b69585
moonlight2329 <zainabahamad048@gmail.com>
2025-10-05 01:24:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import streamlit as st
 
 st.title("zainab's Resume") 
-st.header("Contact Information")
+st.subheader("Contact Information")
 st.write("Email: moonlight@g.com")
 st.write("Phone: 018-2837151")
-st.write("LinkedIn: linkedin.com/in/yourprofile") 
+st.write("LinkedIn: linkedin.com/in/zainab-radzi-a1b1b2368") 
 
 st.header("Education") 
 st.write("BIT. in Information Technology, Universiti Malaysia Kelantan, 2025")
 
 st.header("Work Experience")
 st.write("Software Intern, TechCorp, 2024") 
-st.write("- Built data dashboards using Python and Streamlit")
+st.write("- Built dashboards using Python and Streamlit")
 
 st.header("Skills")
 st.write("- Python")
